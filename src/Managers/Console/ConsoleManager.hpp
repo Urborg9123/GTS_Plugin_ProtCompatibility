76f3f893a00d90ec68ab443f49d818a593a85b98
Urborg9123 <129525994+Urborg9123@users.noreply.github.com>
2026-08-17 18:44:55 +0400

    Add named profile console arguments

--- src/Managers/Console/ConsoleManager.hpp
+++ src/Managers/Console/ConsoleManager.hpp
@@ -5,27 +5,29 @@ namespace GTS {
     class ConsoleManager : public EventListener, public CInitSingleton<ConsoleManager> {
 
         private:
+        using CommandArgs = std::vector<std::string>;
+
         struct Command {
-            std::function<void()> callback = nullptr;
+            std::function<void(const CommandArgs&)> callback = nullptr;
             std::string desc;
-            explicit Command(const std::function<void()>& callback, std::string desc) : callback(callback), desc(std::move(desc)) {}
+            explicit Command(const std::function<void(const CommandArgs&)>& callback, std::string desc) : callback(callback), desc(std::move(desc)) {}
         };
 
         //default base command preffix
         inline static const std::string Default_Preffix = "gts";
         static inline std::unordered_map<std::string, Command> RegisteredCommands = {};
 
-        static void CMD_Help();
-        static void CMD_Version();
-        static void CMD_Unlimited();
-        static void CMD_ProfileDump();
-        static void CMD_ProfileSave();
-        static void CMD_ProfileReset();
-        static void CMD_ProfileLoad();
+        static void CMD_Help(const CommandArgs& args);
+        static void CMD_Version(const CommandArgs& args);
+        static void CMD_Unlimited(const CommandArgs& args);
+        static void CMD_ProfileDump(const CommandArgs& args);
+        static void CMD_ProfileSave(const CommandArgs& args);
+        static void CMD_ProfileReset(const CommandArgs& args);
+        static void CMD_ProfileLoad(const CommandArgs& args);
 
         public:
         static void Init();
-        static void RegisterCommand(std::string_view a_cmdName, const std::function<void()>& a_callback, const std::string& a_desc);
+        static void RegisterCommand(std::string_view a_cmdName, const std::function<void(const CommandArgs&)>& a_callback, const std::string& a_desc);
         static bool Process(const std::string& a_msg);
 
         // Inherited via EventListener
